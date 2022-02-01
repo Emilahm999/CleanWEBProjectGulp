972b6a58ed21607066fe5ae673f24de88d60a5db
Grishchenko Igor <Sherkiosp@gmail.com>
2022-02-01 13:03:47 +0500

    Новая сборка gulp 2022

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();// Вызов функции проверки поддержки webp
+
+// Слайдер
+import Swiper, { Navigation, Pagination } from 'swiper';
