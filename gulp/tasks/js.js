0a5869eae20a5739a2448565f6ef145b6c5dbe77
Sherkios <Sherkios@mail.ru>
2023-08-22 10:10:18 +0500

    Новая версия сборки

---
+++ gulp/tasks/js.js
@@ -0,0 +1,44 @@
+import webpack from "webpack-stream";
+import uglify from "gulp-uglify";
+
+export const js = () => {
+   return app.gulp.src(app.path.src.js, { sourcemaps: true })
+      .pipe(app.plugins.plumber(
+         app.plugins.notify.onError({
+            title: "JS",
+            message: "Error: <%= error.message %>"
+         })
+      ))
+      .pipe(webpack({
+         mode: 'development',
+         devtool: 'eval-source-map',
+         optimization: {
+            minimize: false
+        },
+         output: {
+            filename: 'script.js',
+         }
+      }))
+      .pipe(app.gulp.dest(app.path.build.js))
+      .pipe(app.plugins.browsersync.stream());
+}
+
+export const prodjs = () => {
+   return app.gulp.src(app.path.src.js, { sourcemaps: true })
+      .pipe(app.plugins.plumber(
+         app.plugins.notify.onError({
+            title: "JS",
+            message: "Error: <%= error.message %>"
+         })
+      ))
+      .pipe(webpack({
+         mode: 'production',
+         devtool: false,
+         output: {
+            filename: 'script.js',
+         }
+      }))
+      .pipe(uglify())
+      .pipe(app.gulp.dest(app.path.build.js))
+      .pipe(app.plugins.browsersync.stream());
+}
