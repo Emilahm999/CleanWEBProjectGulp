0222f014ed42aac31907471ed6cc6a9ec380e787
Sherkios <Sherkios@mail.ru>
2023-08-31 16:06:42 +0500

    Добавлена функция проверки на число

--- src/js/ready-functions.js
+++ src/js/ready-functions.js
@@ -21,10 +21,20 @@ export function firstUpperLetter (str , space = false) {
 }
 
 // Возврат случайного числа от min до max (не включая max), при integer вовзращает целые числа
-export function random(min = 0, max = 1 , integer = false) {
+export function random(min, max , integer = false) {
+  if (!isNumber(min)) {
+    console.error(` Аргумент min = ${min} не является числом`);
+    return;
+  }
+  
+  if (!isNumber(max)) {
+    console.error(` Аргумент max = ${max} не является числом`);
+    return;
+  }
+
   if (min > max) {
     console.error(`Минимальное значение больше максимального`);
-    return 
+    return;
   }
 
   let rand = min + Math.random() * (max - min);
@@ -33,5 +43,27 @@ export function random(min = 0, max = 1 , integer = false) {
     return Math.floor(rand);
   } else {
     return rand;
+  }
+}
+// Возвращает истину если перменная равна числу, ложь при ` "", тексте, NaN, undefiend, infinity, -infinity`
+export function isNumber(num) {
+  switch (typeof num) {
+    case "string":
+      if (num.trim() == "") {
+        return false;
+      }
+      return (typeof +num == "number") ? !isNaN(+num) : false;
+    case "boolean":
+      return false;
+    case "number":
+      if (isFinite(num)) {
+        return true;
+      } else if (num === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    default:
+      return false;
   }
 }
