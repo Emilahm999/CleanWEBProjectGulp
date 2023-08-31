f8882d84528651e69c468b95054131f1fac799e5
Sherkios <Sherkios@mail.ru>
2023-08-31 12:55:26 +0500

    Добавлена функция

--- src/js/ready-functions.js
+++ src/js/ready-functions.js
@@ -1,8 +1,8 @@
-// Возврващает строку с большой заглавной буквой, при space все пробелым введенные до строки сохранятся.
+
 export function firstUpperLetter (str , space = false) {
   if (!str) {
-    console.log(`Строка не найдена`);
-    return str
+    console.error(`Строка не найдена`);
+    return
   };
 
   let pos;
@@ -17,5 +17,22 @@ export function firstUpperLetter (str , space = false) {
     return str.slice(0, pos) + str[pos].toUpperCase(pos) + str.slice(pos + 1);
   } else {
     return str[pos].toUpperCase(pos) + str.slice(pos + 1);
+  }
+}
+
+// Возврат случайного числа от min до max (не включая max), при integer вовзращает целые числа
+export function random(min = 0, max = 1 , integer = false) {
+  if (min > max) {
+    console.error(`Минимальное значение больше максимального`);
+    return 
+  }
+
+  let rand = min + Math.random() * (max - min);
+  if (integer) {
+    rand = min + Math.random() * (max + 1 - min);
+    console.log(rand);
+    return Math.floor(rand);
+  } else {
+    return rand;
   }
 }
