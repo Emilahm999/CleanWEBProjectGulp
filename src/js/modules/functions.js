91b6af57c7898ff7ed3be28f09f17425ebcb1442
Igor Grishchenko <igor@antaressoft.ru>
2023-10-18 11:41:03 +0500

    Исправил проверку webp

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,20 +1,21 @@
-export function testWebP(callback) {
+export function isWebP(callback) {
+  
 
-  var webP = new Image();
-  webP.onload = webP.onerror = function () {
-  callback(webP.height == 2);
-  };
-  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-  }
+  function testWebP(callback) {
+    var webP = new Image(); 
+    webP.onload = webP.onerror = function () { 
+      callback(webP.height == 2); 
+    }; 
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
   
-  testWebP(function (support) {
-  
-  if (support == true) {
-  document.querySelector('body').classList.add('webp');
-  }else{
-  document.querySelector('body').classList.add('no-webp');
+    testWebP(function (support) {
+      if (support == true) { document.querySelector('body').classList.add('webp'); 
+    } else {
+      document.querySelector('body').classList.add('no-webp'); 
+    }
+    });
   }
-  });
 
 // Превращает первую букву в строке в заглавную
 export function firstUpperLetter (str , space = false) {
